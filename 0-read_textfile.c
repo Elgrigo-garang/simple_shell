ef1f607f3aa8eb4986682c3fe449f21b29645ce4
Mackay Brian <brianmackay091@gmail.com>
2022-11-16 02:29:42 -0800

    main.h

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -1,12 +1,12 @@
-main.h"
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
- */
+* read_textfile - reads a text file and prints the letters
+* @filename: filename.
+* @letters: numbers of letters printed.
+*
+* Return: numbers of letters printed. It fails, returns 0.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-
+i
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
